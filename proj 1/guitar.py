bd6620dd5dc19898139a6b9f1d9c76d3fe4626b3
mikedigan8 <mcdigan@mavs.coloradomesa.edu>
2020-02-09 11:12:47 -0700

    Second draft of the guitar class

--- proj 1/guitar.py
+++ proj 1/guitar.py
@@ -1,37 +1,57 @@
 class Guitar:
     DEFAULT_STRING_COUNT : int = 6
     DEFAULT_STRING_GAUGE : int = 3221 
-    DEFAULT_PICKUPS : str = 'Single Coil'
-    DEFAULT_TUNING : str = 'EADGBE'
+    DEFAULT_TUNING : str = "E A D G B E"
+    ACCEPTABLE_NOTES : list = ['Ab','A','A#','Bb','B','C','C#','Db','D','D#','Eb','E','F','F#','Gb','G','G#']
+    DEFAULT_VOLUME : float = 0.0
 
-    def __init__(self, make : str, model : str, stringCount : int = DEFAULT_STRING_COUNT, stringGauge : int = DEFAULT_STRING_GAUGE, pickups : str = DEFAULT_PICKUPS, tuning : str = DEFAULT_TUNING):
-      self._make = make
-      self._model = model
+    def __init__(self, tuning : str = DEFAULT_TUNING, stringCount : int = DEFAULT_STRING_COUNT, stringGauge : int = DEFAULT_STRING_GAUGE, volume : float = DEFAULT_VOLUME):
       self._stringCount = stringCount
       self._stringGauge = stringGauge
-      self._pickups = pickups
       self._tuning = tuning
-
-    @property
-    def make(self):
-        return self._make
-
-    @property
-    def model(self):
-        return self._model
+      self._volume = volume
 
     @property
     def stringCount(self):
         return self._stringCount
 
+    @stringCount.setter
+    def stringCount(self, numStrings : int):
+        if numStrings < 0:
+            numStrings = 0
+        self._stringCount = numStrings
+
     @property
     def stringGauge(self):
         return self._stringGauge
-
-    @property
-    def pickups(self):
-        return self._pickups
+    
+    @stringGauge.setter
+    def stringGauge(self, strGauge : int):
+        self._stringGauge = strGauge
 
     @property
     def tuning(self):
         return self._tuning
+
+    @tuning.setter
+    def tuning(self, vals : str):
+        newVals = list(vals.split(" "))
+        if len(newVals) != self._stringCount:
+            vals = self.DEFAULT_TUNING
+        for note in newVals:
+            if note not in self.ACCEPTABLE_NOTES:
+                vals = self.DEFAULT_TUNING
+        self._tuning = vals
+
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @volume.setter
+    def volume(self, vol : float):
+        if (vol < 0):
+            vol = 0
+        elif (vol > 10):
+            vol = 10
+        self._volume = vol
--- proj 1/guitartest.py
+++ proj 1/guitartest.py
@@ -5,8 +5,37 @@ from guitar import Guitar
 class GuitarTest(unittest.TestCase):
 
     def testDefaults(self):
-        guitar = Guitar('Gibson', 'Les Paul')
-        self.assertEqual(guitar.pickups, guitar.DEFAULT_PICKUPS)
+        guitar = Guitar()
         self.assertEqual(guitar.stringCount, guitar.DEFAULT_STRING_COUNT)
         self.assertEqual(guitar.stringGauge, guitar.DEFAULT_STRING_GAUGE)
         self.assertEqual(guitar.tuning, guitar.DEFAULT_TUNING)
+        self.assertEqual(guitar.volume, guitar.DEFAULT_VOLUME)
+
+    def testSetters(self):
+        guitar = Guitar()
+        guitar.stringCount = 8
+        self.assertEqual(guitar.stringCount, 8)
+        guitar.stringCount = -1
+        self.assertEqual(guitar.stringCount, 0)
+        guitar.stringGauge = 2223
+        self.assertEqual(guitar.stringGauge, 2223)
+        guitar.volume = 5.6
+        self.assertEqual(guitar.volume, 5.6)
+        guitar.volume = -3.2
+        self.assertEqual(guitar.volume, 0)
+        guitar.volume = 12
+        self.assertEqual(guitar.volume, 10)
+        guitar.stringCount = 6
+        guitar.tuning = "A# B C D Eb F"
+        self.assertEqual(guitar.tuning, "A# B C D Eb F")
+        guitar.tuning = "A B C"
+        self.assertEqual(guitar.tuning, guitar.DEFAULT_TUNING)
+        guitar.tuning = "Z Z Z Z Z Z"
+        self.assertEqual(guitar.tuning, guitar.DEFAULT_TUNING)
+        guitar.stringCount = 7
+        guitar.tuning = "B E A D G B E"
+        self.assertEqual(guitar.tuning, "B E A D G B E")
+
+
+if __name__ == '__main__':
+    unittest.main()
