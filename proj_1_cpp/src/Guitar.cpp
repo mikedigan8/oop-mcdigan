f211971520c125b94140192849470b30586cb23f
mikedigan8 <mcdigan@mavs.coloradomesa.edu>
2020-02-24 11:39:38 -0700

    Guitar class and tests after tuning validation

--- proj_1_cpp/src/Guitar.cpp
+++ proj_1_cpp/src/Guitar.cpp
@@ -22,7 +22,59 @@ namespace instrument {
         else if(volume > 10) {
             volume = 10.0;
         }
-        //something here to validate the tuning
+        for(int i = 0; i < tuning.length(); i++) {
+            if(tuning[i] != ' ') {
+                //the following two switch statements take and sharp or flat notes and check to make sure
+                //they are real notes. Since C flat is not a real note, I change it to B, since E sharp
+                //is not a real note I change it to F
+                //I also decrease the counter on the string so as to not double count any string
+                if(tuning[i] == 'b') {
+                    countStr--;
+                    switch (tuning[i - 1])
+                    {
+                    case 'C':
+                        tempStr = 'B';
+                        break;
+                    case 'F':
+                        tempStr = 'E';
+                        break;
+                    default:
+                        tempStr = tuning[i - 1];
+                        break;
+                    }
+                }
+                else if(tuning[i] == '#') {
+                    countStr--;
+                    switch (tuning[i - 1])
+                    {
+                        case 'B':
+                            tempStr = 'C';
+                            break;
+                        case 'E':
+                            tempStr = 'F';
+                            break;
+                        default:
+                            tempStr = tuning[i - 1];
+                            break;
+                    }
+                }
+                else {
+                        tempStr = tuning[i];
+                }
+                for(int j = 0; j < 7; j++) {
+                    if(tempStr == ACCEPTABLE_NOTES[j]) notesIn = true;
+                }
+                if(!notesIn) acceptTuning = false;
+                else {
+                    notesIn = false;
+                    tempStr = ' ';
+                    countStr++;
+                }
+            }
+        }
+        if(!acceptTuning || countStr != stringCount) {
+            tuning = DEFAULT_TUNING;
+        }
     }
 
     int Guitar::getStringCount() const {
@@ -47,7 +99,60 @@ namespace instrument {
     }
 
     void Guitar::setTuning(std::string newTuning) {
-        //something here to validate the tuning
+        bool acceptTuning = true;
+        bool notesIn = false;
+        char tempStr;
+        int countStr = 0;
+        for(int i = 0; i < newTuning.length(); i++) {
+            if(newTuning[i] != ' ') {
+                if(newTuning[i] == 'b') {
+                    countStr--;
+                    switch (newTuning[i - 1])
+                    {
+                    case 'C':
+                        tempStr = 'B';
+                        break;
+                    case 'F':
+                        tempStr = 'E';
+                        break;
+                    default:
+                        tempStr = newTuning[i - 1];
+                        break;
+                    }
+                }
+                else if(newTuning[i] == '#') {
+                    countStr--;
+                    switch (newTuning[i - 1])
+                    {
+                        case 'B':
+                            tempStr = 'C';
+                            break;
+                        case 'E':
+                            tempStr = 'F';
+                            break;
+                        default:
+                            tempStr = newTuning[i - 1];
+                            break;
+                    }
+                }
+                else {
+                    tempStr = newTuning[i];
+                }
+                for(int j = 0; j < 7; j++) {
+                    if(tempStr == ACCEPTABLE_NOTES[j]) notesIn = true;
+                }
+                if(!notesIn) acceptTuning = false;
+                else {
+                    notesIn = false;
+                    tempStr = ' ';
+                    countStr++;
+                }
+            }
+        }
+        if(!acceptTuning || countStr != stringCount) {
+            newTuning = DEFAULT_TUNING;
+        }
+        tuning = newTuning;
     }
 
     double Guitar::getVolume() const {
--- proj_1_cpp/src/test_Guitar.cpp
+++ proj_1_cpp/src/test_Guitar.cpp
@@ -26,29 +26,41 @@ TEST(Guitar, BadValues) {
     testVolume = 11;
     guitar.setVolume(testVolume);
     ASSERT_EQ(guitar.getVolume(),10.0);
+
+    string testTuning = "Z Z Z Z Z Z";
+    guitar.setTuning(testTuning);
+    ASSERT_EQ(guitar.getTuning(), guitar.DEFAULT_TUNING);
+
+    testTuning = "A B C";
+    guitar.setTuning(testTuning);
+    ASSERT_EQ(guitar.getTuning(), guitar.DEFAULT_TUNING);
 }
 
 TEST(Guitar, Specific) {
     int testStrings = 7;
     int testGauge = 2223;
-    string testTuning = Guitar::DEFAULT_TUNING;
+    string testTuning = "Ab A A A A A A";
     double testVolume = 5.7;
     Guitar guitar(testStrings, testGauge, testTuning, testVolume);
 
     ASSERT_EQ(guitar.getStringCount(), testStrings);
     ASSERT_EQ(guitar.getStringGauge(), testGauge);
+    ASSERT_EQ(guitar.getTuning(), testTuning);
     ASSERT_EQ(guitar.getVolume(), testVolume);
 
     testStrings = 6;
     testGauge = 2225;
+    testTuning = "Ab B C# D Eb F";
     testVolume = 9.9;
 
     guitar.setStringCount(testStrings);
     guitar.setStringGauge(testGauge);
+    guitar.setTuning(testTuning);
     guitar.setVolume(testVolume);
 
     ASSERT_EQ(guitar.getStringCount(), testStrings);
     ASSERT_EQ(guitar.getStringGauge(), testGauge);
+    ASSERT_EQ(guitar.getTuning(), testTuning);
     ASSERT_EQ(guitar.getVolume(), testVolume);
 }
 
